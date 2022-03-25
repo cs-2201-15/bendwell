3566187cec5482043b1eb8f4476b427d343a2026
Alberto Huerta <albertohuerta@albertohuertamac.lan>
2022-03-25 12:47:54 -0400

    added routines to all stretch view

--- src/Components/AllStretches.js
+++ src/Components/AllStretches.js
@@ -1,17 +1,29 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { setStretches } from "../store/stretches";
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { setRoutines } from '../store/routines';
+import { setStretches } from '../store/stretches';
+import { supabase } from '../supabaseClient';
 
 const AllStretches = () => {
   const dispatch = useDispatch();
   const stretches = useSelector((state) => state.stretches);
+  const routines = useSelector((state) => state.routines);
+  //routine use selector
+
+  let user = supabase.auth.user();
 
   useEffect(() => {
     dispatch(setStretches());
+    dispatch(setRoutines(user.id));
   }, []);
   // you need to add a dependency array (can be empty []) to this useEffect
   // or else it will dispatch over and over continuosly
+
+  //map over
+  // create button / drop down / add to routine
+  // selected routine, submit will fire off thunk with routine id
+
   return (
     <div>
       {console.log(stretches)}
